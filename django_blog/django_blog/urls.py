d3a8fdf896f3132511091391c8f24d327cd1d482
dario sebastian benitez <(benitezdario007@gmail.com)>
2022-12-21 14:28:30 -0300

    paso de la net a la pc

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -10,7 +10,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('admin/', admin.site.urls),
-
     path('inicio/', inicio, name='inicio'),
     path('login/',auth_views.LoginView.as_view(template_name="login.html"), name ='login'),
     path('logout/',auth_views.LogoutView.as_view(),name ='logout'),
